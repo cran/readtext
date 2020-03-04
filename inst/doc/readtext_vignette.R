8df97e7348041f56afac7d32d66745d40421dc22
Kenneth Benoit <kbenoit@lse.ac.uk>
2020-03-04 04:50:02 +0000

    version 0.76

--- inst/doc/readtext_vignette.R
+++ inst/doc/readtext_vignette.R
@@ -1,20 +1,20 @@
-## ----echo = FALSE--------------------------------------------------------
+## ----echo = FALSE-------------------------------------------------------------
 knitr::opts_chunk$set(collapse = TRUE, 
                       comment = "##")
 
-## ----eval=TRUE, message = FALSE------------------------------------------
+## ----eval=TRUE, message = FALSE-----------------------------------------------
 # Load readtext package
 library(readtext)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # Get the data directory from readtext
 DATA_DIR <- system.file("extdata/", package = "readtext")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # Read in all files from a folder
 readtext(paste0(DATA_DIR, "/txt/UDHR/*"))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # Manifestos with docvars from filenames
 readtext(paste0(DATA_DIR, "/txt/EU_manifestos/*.txt"),
          docvarsfrom = "filenames", 
@@ -22,44 +22,44 @@ readtext(paste0(DATA_DIR, "/txt/EU_manifestos/*.txt"),
          dvsep = "_", 
          encoding = "ISO-8859-1")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # Recurse through subdirectories
 readtext(paste0(DATA_DIR, "/txt/movie_reviews/*"))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # Read in comma-separated values
 readtext(paste0(DATA_DIR, "/csv/inaugCorpus.csv"), text_field = "texts")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # Read in tab-separated values
 readtext(paste0(DATA_DIR, "/tsv/dailsample.tsv"), text_field = "speech")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ## Read in JSON data
 readtext(paste0(DATA_DIR, "/json/inaugural_sample.json"), text_field = "texts")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ## Read in Universal Declaration of Human Rights pdf files
 (rt_pdf <- readtext(paste0(DATA_DIR, "/pdf/UDHR/*.pdf"), 
                     docvarsfrom = "filenames", 
                     docvarnames = c("document", "language"),
                     sep = "_"))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ## Read in Word data (.docx)
 readtext(paste0(DATA_DIR, "/word/*.docx"))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # Note: Example required: which URL should we use?
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # Note: Archive file required. The only zip archive included in readtext has 
 # different encodings and is difficult to import (see section 4.2).
 
-## ---- message = FALSE----------------------------------------------------
+## ---- message = FALSE---------------------------------------------------------
 require(quanteda)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # read in comma-separated values with readtext
 rt_csv <- readtext(paste0(DATA_DIR, "/csv/inaugCorpus.csv"), text_field = "texts")
 
@@ -67,11 +67,11 @@ rt_csv <- readtext(paste0(DATA_DIR, "/csv/inaugCorpus.csv"), text_field = "texts
 corpus_csv <- corpus(rt_csv)
 summary(corpus_csv, 5)
 
-## ---- message = FALSE----------------------------------------------------
+## ---- message = FALSE---------------------------------------------------------
 # Load stringi package
 require(stringi)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # Make some text with page numbers
 sample_text_a <- "The quick brown fox named Seamus jumps over the lazy dog also named Seamus, 
 page 1 
@@ -88,7 +88,7 @@ sample_text_a2 <- stri_trim_both(sample_text_a2)
 sample_text_a2 <- sample_text_a2[sample_text_a2 != '']
 stri_paste(sample_text_a2, collapse = '\n')
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 sample_text_b <- "The quick brown fox named Seamus 
 - 1 - 
 jumps over the lazy dog also named Seamus, with 
@@ -105,13 +105,13 @@ sample_text_b2 <- stri_trim_both(sample_text_b2)
 sample_text_b2 <- sample_text_b2[sample_text_b2 != '']
 stri_paste(sample_text_b2, collapse = '\n')
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # create a temporary directory to extract the .zip file
 FILEDIR <- tempdir()
 # unzip file
 unzip(system.file("extdata", "data_files_encodedtexts.zip", package = "readtext"), exdir = FILEDIR)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # get encoding from filename
 filenames <- list.files(FILEDIR, "^(Indian|UDHR_).*\\.txt$")
 
@@ -128,14 +128,14 @@ head(fileencodings)
 notAvailableIndex <- which(!(fileencodings %in% iconvlist()))
 fileencodings[notAvailableIndex]
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 txts <- readtext(paste0(DATA_DIR, "/data_files_encodedtexts.zip"), 
                  encoding = fileencodings,
                  docvarsfrom = "filenames", 
                  docvarnames = c("document", "language", "input_encoding"))
 print(txts, n = 50)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 corpus_txts <- corpus(txts)
 summary(corpus_txts, 5)
 
